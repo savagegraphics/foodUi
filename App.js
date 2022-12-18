1759fbda17f612fe7f3edc0ed11c1ed5604a4960
savagegraphics <olawaleraji188@gmail.com>
2022-12-17 18:46:15 -0600

    first commit

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import RecipeDetailScreen from "./Screens/Restaurant/RecipeDetailScreen";
 import DATA from "./config/Restaurant/DATA";
 const App = () => {
-  return <RecipeDetailScreen recipe={DATA[0].recipes[1]} />;
+  return <RecipeDetailScreen recipe={ DATA[ 0 ].recipes[ 1 ] } />;
 };
 
 export default App;
